52f459e043369853b07598c4a9731241410f2477
SMaibuecher <smaibuecher@stroeer.de>
2023-09-07 16:28:52 +0200

    fix bug

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from fastapi import FastAPI
 
-from db.db_operations import select_count_from_first_row, increment_table_counter, create_table_and_insert_row
+from db.db_operations import (
+    select_count_from_first_row,
+    increment_table_counter,
+    create_table_and_insert_row,
+)
+
+
+# TODO nochmal gucken, ob das jetzt mit den gloabls funktioniert. und docker übernimmt die Änderungen nicht
+
+class GlobalVariables:
+    def __int__(self):
+        print("GlobalVariables set up")
+
+
+_globals = GlobalVariables()
+_globals.db_is_online = False
+
 
 app = FastAPI()
 
+
 @app.get("/")
 def endpoint_a_root():
+    if not _globals.db_is_online:
+        create_table_and_insert_row()
+        _globals.db_is_online = True
     count = select_count_from_first_row()
     increment_table_counter(count)
     return {"message": f"Endpoint was called {count} times"}
-
-
-@app.get("/ct/")
-def endpoint_a_root():
-    create_table_and_insert_row()
-    return {"message": f"Table initialized"}
